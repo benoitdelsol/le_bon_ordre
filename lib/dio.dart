595973ca023bfe67b344acbe63cdd00ed179ea87
Bun <dusoleil01@hotmail.fr>
2023-11-13 17:10:16 +0100

    début du back

--- lib/dio.dart
+++ lib/dio.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:le_bon_ordre/questionClass.dart';
+
 
 /// More examples see https://github.com/cfug/dio/blob/main/example
 void main() async {
   final dio = Dio();
-  final response = await dio.get('https://pub.dev');
-  print(response.data);
+  final response = await dio.get('http://localhost:8080/questions');
+  var questionList = jsonDecode(response.data);
+  List<Question> questions= [Question(jsonDecode(questionList[0][0]), jsonDecode(questionList[0][1]), [jsonDecode(questionList[0][2][0]),jsonDecode(questionList[0][2][1]),jsonDecode(questionList[0][2][2]),jsonDecode(questionList[0][2][3]),jsonDecode(questionList[0][2][4])]),Question(questionList[1][0][0], questionList[1][1][0], [questionList[1][2][0],questionList[1][2][1],questionList[1][2][2],questionList[1][2][3],questionList[1][2][4]])];
+  print(questions);
 }
